6619181c51437d54241934b51f35a99dd205acb1
Dynesshely <catrol@qq.com>
2022-09-11 21:39:10 +0800

    💾 📝 Feat, Chore: 黑神话: 悟空 内测版 反馈工具

---
+++ 反馈工具/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace 反馈工具
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Send(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("反馈信息发送成功", "反馈工具", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
